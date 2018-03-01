36e4cb8b24dd4c91402bf5efd64f992a20daf3cc
KimSia Sim <kimcity@gmail.com>
2018-03-01 15:29:56 +0800

    attempt to write a proper test

--- examples/c71_create_empty_excel_file/openpyxl/index.py
+++ examples/c71_create_empty_excel_file/openpyxl/index.py
@@ -5,6 +5,3 @@ def save_empty_file():
     book = Workbook()
     save_as_path = 'empty.xlsx'
     book.save(save_as_path)
-
-
-save_empty_file()
--- tests/test_7_1_openpyxl.py
+++ tests/test_7_1_openpyxl.py
@@ -1,6 +1,26 @@
+from pathlib import Path
+from examples.c71_create_empty_excel_file.openpyxl import index
 import unittest
 
+
 class TestOpenPyXLEmptyFile(unittest.TestCase):
+    def setUp(self):
+        dir_path = Path('../examples/c71_create_empty_excel_file/openpyxl/')
+        for f in dir_path.glob('*.xlsx'):
+            f.unlink()
+
+    def tearDown(self):
+        dir_path = Path('../examples/c71_create_empty_excel_file/openpyxl/')
+        for f in dir_path.glob('*.xlsx'):
+            f.unlink()
+
     def test(self):
-        pass
-        # index.save_empty_file()
+        self.assertFalse(
+            Path(
+                '../examples/c71_create_empty_excel_file/openpyxl/empty.xlsx'
+            ).is_file())
+        index.save_empty_file()
+        self.assertTrue(
+            Path(
+                '../examples/c71_create_empty_excel_file/openpyxl/empty.xlsx'
+            ).is_file())
