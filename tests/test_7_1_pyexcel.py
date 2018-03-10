96c6fd9a625971c954a47b884cdcf4d42546719d
KimSia Sim <kimcity@gmail.com>
2018-03-10 13:57:55 +0800

    add in the test for pyexcel
    ref #4

---
+++ tests/test_7_1_pyexcel.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from examples.c71_create_empty_excel_file.pyexcel import index
+import unittest
+import os
+
+
+class TestOpenPyXLEmptyFile(unittest.TestCase):
+    def setUp(self):
+        dir_path = Path(__file__).resolve().parent
+        for f in dir_path.glob('*.xlsx'):
+            f.unlink()
+
+    def tearDown(self):
+        dir_path = Path(__file__).resolve().parent
+        for f in dir_path.glob('*.xlsx'):
+            f.unlink()
+
+    def test(self):
+        # python35 cannot write to Path, so use os.path
+        save_path = os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            'c71_pyexcel_empty.xlsx')
+        self.assertFalse(
+            Path(save_path).is_file())
+        index.save_empty_file(save_path)
+        self.assertTrue(
+            Path(save_path).is_file())
