6de75ef9e46d5db3bf65ac32ec0ce718c0e49023
KimSia Sim <kimcity@gmail.com>
2018-03-10 13:57:29 +0800

    now pyexcel works to create empty excel file
    closed #4

--- examples/c71_create_empty_excel_file/openpyxl/index.py
+++ examples/c71_create_empty_excel_file/openpyxl/index.py
@@ -1,6 +1,6 @@
 from openpyxl import Workbook
 
 
-def save_empty_file(save_as_path = 'c71_openpyxl_empty.xlsx'):
+def save_empty_file(save_as_path='c71_openpyxl_empty.xlsx'):
     book = Workbook()
     book.save(save_as_path)
