048f198b9c682f12e9ed095fa38f1755ab6619c0
KimSia Sim <kimcity@gmail.com>
2018-03-12 21:36:19 +0800

    sanitise and validate sheet name
    closed #7

--- examples/c72_rename_validate_sheet_name/custom/index.py
+++ examples/c72_rename_validate_sheet_name/custom/index.py
@@ -1,5 +1,18 @@
+import re
+
+
 def validate_sheet_name(sheet_name):
+    if len(sheet_name) > 33:
+        return False
+    return not re.search("[%#&/\*\?\\\]", sheet_name)
 
 
 def sanitise_sheet_name(sheet_name):
-  
+
+    replaced_with_empty = ["%", "#", "&", "*", "?"]
+    replaced_with_hyphens = ["/", "\\"]
+    for ch in replaced_with_empty:
+        sheet_name = sheet_name.replace(ch, "")
+    for ch in replaced_with_hyphens:
+        sheet_name = sheet_name.replace(ch, "-")
+    return sheet_name[0:33]
