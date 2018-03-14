90d63c9e331e4a6fe81149a617ffe599c985917c
KimSia Sim <kimcity@gmail.com>
2018-03-14 18:45:25 +0800

    remove the lines inside index.py and move it to notes.md

--- examples/c80_read_from_range/openpyxl/index.py
+++
@@ -1,2 +0,0 @@
-http://openpyxl.readthedocs.io/en/stable/tutorial.html#accessing-many-cells
-https://stackoverflow.com/q/34578910/80353
---
+++ examples/c80_read_from_range/openpyxl/notes.md
@@ -0,0 +1,2 @@
+http://openpyxl.readthedocs.io/en/stable/tutorial.html#accessing-many-cells
+https://stackoverflow.com/q/34578910/80353
