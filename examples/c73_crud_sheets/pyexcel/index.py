2c6f09d78cceacd9c3179c74535005dadae055d7
KimSia Sim <kimcity@gmail.com>
2018-03-17 14:10:01 +0800

    rename sheet and order sheets work inclusive of unit tests for pyexcel
    ref #9

--- examples/c73_crud_sheets/pyexcel/index.py
+++ examples/c73_crud_sheets/pyexcel/index.py
@@ -1,5 +1,8 @@
 import pyexcel
 from pyexcel import Sheet
+from collections import OrderedDict
+from examples.c72_rename_validate_sheet_name.custom \
+    import index as rename_index
 
 
 def list_all_sheet_names(file_path='../3_sheets.xlsx'):
@@ -28,11 +31,35 @@ def delete_sheet(file_path, sheet_name):
     return book.save_as(file_path)
 
 
-def order_sheets_alphabetically(file_path, order="asc"):
-    book = Workbook()
-    book.save(file_path)
+def order_sheets_alphabetically(
+    file_path,
+    dest_file_path,
+    reverse=False
+):
+    book = pyexcel.get_book(file_name=file_path)
+    sheet_names = book.sheet_names()
+    sheet_names.sort(reverse=reverse)
+    data = OrderedDict()
+    for sheet_name in sheet_names:
+        data.update({sheet_name: book[sheet_name]})
+    return pyexcel.save_book_as(
+        bookdict=data, dest_file_name=dest_file_path)
 
 
-def rename_sheet(file_path, sheet_name, new_sheet_name):
-    book = Workbook()
-    book.save(file_path)
+def rename_sheet(file_path, dest_file_path, sheet_name, new_sheet_name):
+    if (not has_sheet(file_path, sheet_name)):
+        raise Exception
+    if (has_sheet(file_path, new_sheet_name)):
+        raise Exception
+    new_sheet_name = rename_index.sanitise_sheet_name(new_sheet_name)
+    sheet_names = list_all_sheet_names(file_path)
+    book = pyexcel.get_book(file_name=file_path)
+    data = OrderedDict()
+    for original_sheet_name in sheet_names:
+        saved_as = original_sheet_name
+        if original_sheet_name == sheet_name:
+            saved_as = new_sheet_name
+        data.update({saved_as: book[original_sheet_name]})
+    return pyexcel.save_book_as(
+        bookdict=data, dest_file_name=dest_file_path
+    )
