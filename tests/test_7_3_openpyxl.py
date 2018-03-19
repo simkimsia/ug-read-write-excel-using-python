be4717e4f795eb88ce1ec8e13a0a9121a2b5dce6
KimSia Sim <kimcity@gmail.com>
2018-03-19 09:29:23 +0800

    openpyxl add new sheet and check if sheet exists
    ref #9

--- tests/test_7_3_openpyxl.py
+++ tests/test_7_3_openpyxl.py
@@ -32,4 +32,36 @@ class TestOpenPyXLCrud(unittest.TestCase):
         expected_sheets = ['Sheet1', 'Sheet2', 'Sheet3']
         self.assertListEqual(sheets, expected_sheets)
 
-    
+    def test_has_sheet(self):
+        # python35 cannot write to Path, so use os.path
+        tests_path = os.path.dirname(
+            os.path.abspath(__file__))
+        project_root = os.path.dirname(
+            tests_path)
+        examples_path = os.path.join(project_root, 'examples')
+        sample_xlsx_path = os.path.join(
+            examples_path, 'c73_crud_sheets', '3_sheets.xlsx')
+        self.assertTrue(
+            Path(sample_xlsx_path).is_file())
+        self.assertTrue(index.has_sheet(sample_xlsx_path, 'Sheet1'))
+        self.assertFalse(index.has_sheet(sample_xlsx_path, 'Does not exist'))
+
+    def test_add_new_sheet(self):
+        # python35 cannot write to Path, so use os.path
+        save_path = os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            'c73_openpyxl_empty.xlsx')
+        self.assertFalse(
+            Path(save_path).is_file())
+        create_index.save_empty_file_another_way(save_path)
+        self.assertTrue(
+            Path(save_path).is_file())
+        new_sheet_name = 'New Sheet'
+        self.assertFalse(index.has_sheet(save_path, new_sheet_name))
+        index.add_new_sheet(save_path, new_sheet_name)
+        self.assertTrue(index.has_sheet(save_path, new_sheet_name))
+        # when attempt to add new sheet name clashes with existing
+        # exception is thrown
+        with self.assertRaises(Exception):
+            index.add_new_sheet(save_path, new_sheet_name)
