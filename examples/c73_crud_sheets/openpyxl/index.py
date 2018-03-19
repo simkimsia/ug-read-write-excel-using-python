9fd41df3d3238ea8f9f3369cc4a53dfe027ab898
KimSia Sim <kimcity@gmail.com>
2018-03-19 09:29:23 +0800

    openpyxl add new sheet and check if sheet exists
    ref #9

--- examples/c73_crud_sheets/openpyxl/index.py
+++ examples/c73_crud_sheets/openpyxl/index.py
@@ -6,9 +6,15 @@ def list_all_sheet_names(file_path='../3_sheets.xlsx'):
     return wb.sheetnames
 
 
+def has_sheet(file_path, looking_for_sheet_name):
+    sheet_names = list_all_sheet_names(file_path)
+    return looking_for_sheet_name in sheet_names
+
+
 def add_new_sheet(file_path, new_sheet_name):
-    book = Workbook()
-    book.save(file_path)
+    wb = load_workbook(filename=file_path)
+    wb.create_sheet(new_sheet_name)
+    return wb.save(file_path)
 
 
 def delete_sheet(file_path, sheet_name):
