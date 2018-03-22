f5927160e77ff1d0c3e02182f534926ed613a0e5
KimSia Sim <kimcity@gmail.com>
2018-03-22 10:17:31 +0800

    convert from coordinate string to indices work for openpyxl
    ref #11

--- tests/test_8_0_openpyxl.py
+++ tests/test_8_0_openpyxl.py
@@ -1,26 +1,30 @@
 from pathlib import Path
 from examples.c80_convert_indices_coordinates.openpyxl import index
-from examples.c71_create_empty_excel_file.openpyxl \
-    import index as create_index
 import unittest
-import os
 
 
 class TestOpenPyXLIndicesCoordinates(unittest.TestCase):
-    def setUp(self):
-        dir_path = Path(__file__).resolve().parent
-        for f in dir_path.glob('*.xlsx'):
-            f.unlink()
-
-    def tearDown(self):
-        dir_path = Path(__file__).resolve().parent
-        for f in dir_path.glob('*.xlsx'):
-            f.unlink()
 
     def test_coordinates_to_indices(self):
         # python35 cannot write to Path, so use os.path
         coordinate_string = 'A4'
         zero_based = True
         expected_indices = (0, 3)
+        indices = index.coordinate_to_index(coordinate_string, zero_based)
+        self.assertEqual(indices, expected_indices)
+
+        zero_based = False
+        expected_indices = (1, 4)
+        indices = index.coordinate_to_index(coordinate_string, zero_based)
+        self.assertEqual(indices, expected_indices)
+
+        coordinate_string = 'AB24'
+        zero_based = True
+        expected_indices = (27, 23)
+        indices = index.coordinate_to_index(coordinate_string, zero_based)
+        self.assertEqual(indices, expected_indices)
+
+        zero_based = False
+        expected_indices = (28, 24)
         indices = index.coordinate_to_index(coordinate_string, zero_based)
         self.assertEqual(indices, expected_indices)
