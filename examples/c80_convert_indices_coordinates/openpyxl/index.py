b20cf7b700684754bd60ce4fbff809778e7eb792
KimSia Sim <kimcity@gmail.com>
2018-03-22 10:27:25 +0800

    using openpyxl to do the conversion between coordinate_string and indices which are tuple-like
    closed #11

--- examples/c80_convert_indices_coordinates/openpyxl/index.py
+++ examples/c80_convert_indices_coordinates/openpyxl/index.py
@@ -1,4 +1,6 @@
-from openpyxl.utils import coordinate_from_string, column_index_from_string
+from openpyxl.utils import \
+    coordinate_from_string, column_index_from_string, \
+    get_column_letter
 
 
 def coordinate_to_index(coordinate_string, zero_based=True):
@@ -11,3 +13,13 @@ def coordinate_to_index(coordinate_string, zero_based=True):
         col = col - 1
         row = row - 1
     return (col, row)
+
+
+def index_to_coordinate(index_tuple, zero_based=True):
+    col = index_tuple[0]
+    row = index_tuple[1]
+    if zero_based:
+        col = col + 1
+        row = row + 1
+    col_letters = get_column_letter(col)
+    return str(col_letters) + str(row)
--- tests/test_8_0_openpyxl.py
+++ tests/test_8_0_openpyxl.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from examples.c80_convert_indices_coordinates.openpyxl import index
 import unittest
 
@@ -6,7 +5,6 @@ import unittest
 class TestOpenPyXLIndicesCoordinates(unittest.TestCase):
 
     def test_coordinates_to_indices(self):
-        # python35 cannot write to Path, so use os.path
         coordinate_string = 'A4'
         zero_based = True
         expected_indices = (0, 3)
@@ -28,3 +26,26 @@ class TestOpenPyXLIndicesCoordinates(unittest.TestCase):
         expected_indices = (28, 24)
         indices = index.coordinate_to_index(coordinate_string, zero_based)
         self.assertEqual(indices, expected_indices)
+
+    def test_indices_to_coordinates(self):
+        indices = (1, 4)
+        zero_based = False
+        expected_coordinate_string = 'A4'
+        coordinate_string = index.index_to_coordinate(indices, zero_based)
+        self.assertEqual(coordinate_string, expected_coordinate_string)
+
+        zero_based = True
+        expected_coordinate_string = 'B5'
+        coordinate_string = index.index_to_coordinate(indices, zero_based)
+        self.assertEqual(coordinate_string, expected_coordinate_string)
+
+        indices = (28, 24)
+        zero_based = False
+        expected_coordinate_string = 'AB24'
+        coordinate_string = index.index_to_coordinate(indices, zero_based)
+        self.assertEqual(coordinate_string, expected_coordinate_string)
+
+        zero_based = True
+        expected_coordinate_string = 'AC25'
+        coordinate_string = index.index_to_coordinate(indices, zero_based)
+        self.assertEqual(coordinate_string, expected_coordinate_string)
