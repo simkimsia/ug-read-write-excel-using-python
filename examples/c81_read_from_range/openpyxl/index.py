f6dae33b4ba1c8bdd0607645d746fdd697e8a3cc
KimSia Sim <kimcity@gmail.com>
2018-03-26 10:08:38 +0800

    tests for read from range using openpyxl

--- examples/c81_read_from_range/openpyxl/index.py
+++ examples/c81_read_from_range/openpyxl/index.py
@@ -1,7 +1,7 @@
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook
 
 
 def read_from_range(file_path, sheet_name, start_cell, end_cell):
-    wb = load_workbook(file_path, use_iterators=True)
+    wb = load_workbook(file_path)
     ws = wb[sheet_name]
     return ws[start_cell:end_cell]
