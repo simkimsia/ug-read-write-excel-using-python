ed37b019ac4c7403cf0be45e05c2dfe813d04cc2
KimSia Sim <kimcity@gmail.com>
2018-03-28 12:02:09 +0800

    write to range for openpyxl and test pass

---
+++ examples/c82_write_to_range/notes.md
@@ -0,0 +1,1 @@
+https://yagisanatode.com/2017/11/18/copy-and-paste-ranges-in-excel-with-openpyxl-and-python-3/
---
+++ examples/c82_write_to_range/openpyxl/index.py
@@ -0,0 +1,23 @@
+from openpyxl import load_workbook
+from examples.c80_convert_indices_coordinates.openpyxl \
+    import index as convert_index
+
+
+def write_to_range(file_path, sheet_name, start_cell, end_cell, data):
+    # data is expected to be 2 dimensional list
+    start_cell_index = convert_index.coordinate_to_index(start_cell, False)
+    end_cell_index = convert_index.coordinate_to_index(end_cell, False)
+    start_col = start_cell_index[0]
+    start_row = start_cell_index[1]
+    end_col = end_cell_index[0]
+    end_row = end_cell_index[1]
+    wb = load_workbook(file_path)
+    ws = wb[sheet_name]
+    count_row = 0
+    for i in range(start_row, end_row+1):
+        count_col = 0
+        for j in range(start_col, end_col+1):
+            ws.cell(row=i, column=j).value = data[count_row][count_col]
+            count_col += 1
+        count_row += 1
+    return wb.save(file_path)
