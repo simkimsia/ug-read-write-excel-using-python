b55dcc3a267a13f2e34a4e0acc318ce933186729
KimSia Sim <kimcity@gmail.com>
2018-03-30 11:17:13 +0800

    added freeze panes for openpyxl

---
+++ examples/c83_freeze_panes/openpyxl/index.py
@@ -0,0 +1,23 @@
+from openpyxl import load_workbook
+
+
+def has_freeze_pane(file_path, sheet_name):
+    wb = load_workbook(file_path)
+    ws = wb[sheet_name]
+    if ws.freeze_panes == 'A1' or ws.freeze_panes is None:
+        return False
+    return True
+
+
+def freeze_pane(file_path, sheet_name, cell_or_string):
+    wb = load_workbook(file_path)
+    ws = wb[sheet_name]
+    ws.freeze_panes = cell_or_string
+    return wb.save_as(file_path)
+
+
+def unfreeze_pane(file_path, sheet_name, cell_or_string):
+    wb = load_workbook(file_path)
+    ws = wb[sheet_name]
+    ws.freeze_panes = None
+    return wb.save_as(file_path)
