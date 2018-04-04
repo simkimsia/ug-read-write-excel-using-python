1d232be423bcf1978def6b1e5d23bb229263692f
KimSia Sim <kimcity@gmail.com>
2018-04-04 09:51:14 +0800

    add font size functions and tests

--- examples/c91_font_styles/openpyxl/index.py
+++ examples/c91_font_styles/openpyxl/index.py
@@ -8,3 +8,12 @@ def set_font_color_red(workbook):
     a1.font = ft
 
     return a1
+
+
+def set_font_size(workbook, size):
+    ws = workbook.active
+    a1 = ws['A1']
+    ft = Font(size=size)
+    a1.font = ft
+
+    return a1
--- tests/test_9_1_openpyxl.py
+++ tests/test_9_1_openpyxl.py
@@ -4,7 +4,7 @@ from openpyxl import Workbook
 from base_test_cases import ExcelTest
 
 
-class TestOpenPyXLWriteToRange(ExcelTest):
+class TestOpenPyXLFontStyles(ExcelTest):
 
     def test_font_color(self):
         wb = Workbook()
@@ -18,3 +18,14 @@ class TestOpenPyXLWriteToRange(ExcelTest):
         self.assertEqual(a1.font.color.rgb, "FF000000")
         a1 = index.set_font_color_red(wb)
         self.assertEqual(a1.font.color.rgb, colors.RED)
+
+    def test_font_size(self):
+        wb = Workbook()
+        ws = wb.active
+        a1 = ws['A1']
+        a1_font = a1.font
+        default_size = 11
+        self.assertEqual(a1_font.size, default_size)
+        new_size = 20
+        a1 = index.set_font_size(wb, new_size)
+        self.assertEqual(a1.font.size, new_size)
