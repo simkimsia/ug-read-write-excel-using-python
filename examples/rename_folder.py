1a527de39c5dc6e6bf3572920db28915bd1f4bb1
KimSia Sim <kimcity@gmail.com>
2018-04-15 14:16:25 +0800

    rename the test files and also add in set print area and test

--- examples/rename_folder.py
+++ examples/rename_folder.py
@@ -21,21 +21,30 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 test_path = os.path.join(os.path.dirname(current_dir), 'tests')
 test_path_py = os.path.join(test_path, '*.py')
 
-for fn in os.listdir(current_dir):
-    if not os.path.isdir(os.path.join(current_dir, fn)):
-        # Not a directory
-        continue
-    if fn.startswith('c0'):
-        for filename in glob.iglob(test_path_py, recursive=True):
-            with open(filename, 'r+') as f:
-                for line in f:
-                    searchFor = 'c07_' + fn[4:]
-                    if searchFor in line:
-                        f.write(line.replace(searchFor, fn))
-                    searchFor = 'c08_' + fn[4:]
-                    if searchFor in line:
-                        f.write(line.replace(searchFor, fn))
-                    searchFor = 'c9' + fn[4:]
-                    if searchFor in line:
-                        f.write(line.replace(searchFor, fn))
-            f.close()
+for filename in glob.iglob(test_path_py, recursive=True):
+    print(filename)
+    basename = os.path.basename(filename)
+    if basename.startswith('test_9'):
+        new_name = 'test_09' + basename[6:]
+        os.rename(filename,
+                  os.path.join(test_path, new_name))
+
+
+# for fn in os.listdir(current_dir):
+#     if not os.path.isdir(os.path.join(current_dir, fn)):
+#         # Not a directory
+#         continue
+#     if fn.startswith('c0'):
+#         for filename in glob.iglob(test_path_py, recursive=True):
+#             with open(filename, 'r+') as f:
+#                 for line in f:
+#                     searchFor = 'c07_' + fn[4:]
+#                     if searchFor in line:
+#                         f.write(line.replace(searchFor, fn))
+#                     searchFor = 'c08_' + fn[4:]
+#                     if searchFor in line:
+#                         f.write(line.replace(searchFor, fn))
+#                     searchFor = 'c9' + fn[4:]
+#                     if searchFor in line:
+#                         f.write(line.replace(searchFor, fn))
+#             f.close()
---
+++ tests/test_10_1_openpyxl.py
@@ -0,0 +1,28 @@
+from examples.c10_1_set_print_area.openpyxl import index
+from examples.c07_1_create_empty_excel_file.openpyxl \
+    import index as create_index
+from openpyxl import load_workbook
+from pathlib import Path
+import os
+from base_test_cases import ExcelTest
+
+
+class TestOpenPyXLPrintArea(ExcelTest):
+
+    def test_set_print_area(self):
+        tests_path = os.path.dirname(
+            os.path.abspath(__file__))
+        write_xlsx_path = os.path.join(
+            tests_path, 'set_print_area.xlsx')
+        create_index.save_empty_file(write_xlsx_path)
+        self.assertTrue(
+            Path(write_xlsx_path).is_file())
+
+        workbook = load_workbook(write_xlsx_path)
+        worksheet = workbook.active
+
+        index.set_print_area(write_xlsx_path, 'A1:U50')
+        wb_after_set_print_area = load_workbook(write_xlsx_path)
+        ws_after_set_print_area = wb_after_set_print_area.active
+        expected_result = ['$A$1:$U$50']
+        self.assertEqual(ws_after_set_print_area.print_area, expected_result)
